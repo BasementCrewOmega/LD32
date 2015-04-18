d25a8ed83a220efb0fbf6d52f553d20e42cdac83
Kevin <kprehn01@gmail.com>
2015-04-18 00:28:45 -0500

    See the above commit

---
+++ src/main/java/com/basementcrew/ld32/states/TimedGameState.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.basementcrew.ld32.states;
+
+import bropals.lib.simplegame.state.GameState;
+
+/**
+ *
+ * @author Kevin
+ */
+public class TimedGameState extends GameState {
+
+    public void update(long dt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public void render(Object o) {
+    }
+
+    @Override
+    public void onEnter() {
+    }
+
+    @Override
+    public void onExit() {
+    }
+
+}
---
+++ src/main/java/com/basementcrew/ld32/states/TimedGameStateRunner.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.basementcrew.ld32.states;
+
+import bropals.lib.simplegame.GameStateRunner;
+import bropals.lib.simplegame.GameWindow;
+import bropals.lib.simplegame.io.AssetManager;
+import bropals.lib.simplegame.state.GameState;
+
+/**
+ *
+ * @author Kevin
+ */
+public class TimedGameStateRunner extends GameStateRunner {
+
+    public TimedGameStateRunner(GameWindow window, AssetManager assetManager) {
+        super(window, assetManager);
+    }
+
+    @Override
+    public void loop() {
+        boolean running = true;
+        while(running) {
+            if (getCurrentState() == null || getCurrentWindow() == null || 
+                    getCurrentState().isRequestingToClose()) {
+                running = false;
+                continue;
+            }
+            
+            setStartTime(System.currentTimeMillis());
+            GameState runState = getCurrentState(); // in case the state is changed
+                                              // in the middle of the loop
+            getCurrentWindow().flushInput();
+            runState.update();
+            renderState(runState);
+            diff = System.currentTimeMillis() - startTime;
+            if (diff < millisBetweenFrames) {
+                try {
+                    Thread.sleep(millisBetweenFrames - diff);
+                } catch(Exception e) {}
+            }
+        }
+        currentWindow.destroy();
+    }
+    
+    
+    
+}

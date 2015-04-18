18523dab457b49b6fd295ebba33d70f98efb8828
Kevin <kprehn01@gmail.com>
2015-04-18 00:27:16 -0500

    Starting to add in some better timing

--- src/main/java/com/basementcrew/ld32/ApplicationMain.java
+++ src/main/java/com/basementcrew/ld32/ApplicationMain.java
@@ -1,9 +1,19 @@
 package com.basementcrew.ld32;
 
+import bropals.lib.simplegame.AWTGameWindow;
+import bropals.lib.simplegame.GameStateRunner;
+import bropals.lib.simplegame.GameWindow;
+import bropals.lib.simplegame.io.AssetManager;
+import java.io.File;
+
 public class ApplicationMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+            AssetManager assetManager = new AssetManager(ApplicationMain.class, true);
+            
+            GameWindow window = new AWTGameWindow("Ludum Dare 32", 800, 600);
+            GameStateRunner runner = new GameStateRunner(window, assetManager);
+            runner.loop();
 
 	}
 
